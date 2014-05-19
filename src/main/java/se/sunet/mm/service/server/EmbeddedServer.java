f1ead38922b4b81793cdf0e4ed6999be73c69dab
Johan Lundberg <lundberg@nordu.net>
2014-05-19 18:14:09 +0200

    Started to add ssl support for embedded server.

--- src/main/java/se/sunet/mm/service/server/EmbeddedServer.java
+++ src/main/java/se/sunet/mm/service/server/EmbeddedServer.java
@@ -1,8 +1,10 @@
 package se.sunet.mm.service.server;
 
-import org.eclipse.jetty.server.Server;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.servlet.ServletContainer;
 
@@ -25,22 +27,59 @@ public class EmbeddedServer {
         return prop;
     }
 
-    private ServletContextHandler getContext(String packagesLocation, String rootPath) {
+    private ServletContextHandler getServletContext(String packagesLocation, String rootPath) {
         ServletHolder sh = new ServletHolder(ServletContainer.class);
         sh.setInitParameter(ServerProperties.PROVIDER_PACKAGES, packagesLocation);
-        ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(sh, rootPath);
         return context;
+    }
+
+    private SslContextFactory getSslContextFactory(String keyStorePath, String keyStorePassword, String keyManagerPassword) {
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(keyStorePath);
+        sslContextFactory.setKeyStorePassword(keyStorePassword);
+        sslContextFactory.setKeyManagerPassword(keyManagerPassword);
+        return sslContextFactory;
+    }
+
+    private Server getSslServer(String host, Integer port, SslContextFactory sslContextFactory) {
+        Server server = new Server();
+        HttpConfiguration https_config = new HttpConfiguration();
+        https_config.setSecureScheme("https");
+        https_config.setSecurePort(port);
+        https_config.addCustomizer(new SecureRequestCustomizer());
+        HttpConnectionFactory httpConnectionFactory = new HttpConnectionFactory(https_config);
+        SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(sslContextFactory,"http/1.1");
+        ServerConnector https = new ServerConnector(server, sslConnectionFactory, httpConnectionFactory);
+        https.setHost(host);
+        https.setPort(port);
+        https.setIdleTimeout(500000);
+        server.addConnector(https);
+        return server;
     }
 
     public void start(String configFile) throws Exception {
         Properties prop = getProperties(configFile);
         String host = prop.getProperty("host");
         Integer port = Integer.parseInt(prop.getProperty("port"));
-        InetSocketAddress address = new InetSocketAddress(host, port);
-        server = new Server(address);
-        ServletContextHandler context = getContext("se.sunet.mm.service.api", "/*");
-        server.setHandler(context);
+        String packagesLocation = prop.getProperty("apiPackagesPath");
+        String rootPath = prop.getProperty("apiRootPath");
+        Boolean https = Boolean.parseBoolean(prop.getProperty("https"));
+
+        if (!https) {
+            InetSocketAddress address = new InetSocketAddress(host, port);
+            server = new Server(address);
+        } else {
+            String keyStorePath = prop.getProperty("keyStorePath");
+            String keyStorePassword = prop.getProperty("keyStorePassword");
+            String keyManagerPassword = prop.getProperty("keyManagerPassword");
+            SslContextFactory sslContextFactory = getSslContextFactory(keyStorePath, keyStorePassword, keyManagerPassword);
+            server = getSslServer(host, port, sslContextFactory);
+        }
+
+        ServletContextHandler servletContext = getServletContext(packagesLocation, rootPath);
+        server.setHandler(servletContext);
         server.start();
         server.join();
     }
